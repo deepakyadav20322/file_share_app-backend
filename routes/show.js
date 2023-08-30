ec6994619d71b0e6d756c5dcc7aaa7ad3dc6df47
deepak <informationtechnology20322@@gmail.com>
2023-08-30 16:45:11 +0530

    directally put renser.com base url b/c ejs file

--- routes/show.js
+++ routes/show.js
@@ -9,7 +9,7 @@ router.get('/:uuid', async (req, res) => {
             return res.render('download', { error: 'Link has been expired.'});
         } 
         // return res.render('download', { uuid: file.uuid, fileName: file.filename, fileSize: file.size, downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}` });
-        return res.render('download', { uuid: file.uuid, fileName: file.filename, fileSize: file.size, downloadLink: `${process.env.LIVE_APP_BASE_URL}/files/download/${file.uuid}` });
+        return res.render('download', { uuid: file.uuid, fileName: file.filename, fileSize: file.size, downloadLink: `https://file-share-app-backend.onrender.com/files/download/${file.uuid}` });
     } catch(err) {
         return res.render('download', { error: 'Something went wrong.'});
     }
